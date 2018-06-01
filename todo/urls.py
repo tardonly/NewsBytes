15b33be3095b8452eeef663bc6f1d1d70c333447
Ram Pratap <31067088+tardonly@users.noreply.github.com>
2018-06-01 11:25:35 +0530

    Update urls.py

--- todo/urls.py
+++ todo/urls.py
@@ -7,7 +7,6 @@ urlpatterns = [
     path('add', views.addTodo, name='add'),
     path('complete/<todo_id>', views.completeTodo, name='complete'),
     path('deletecomplete', views.deleteCompleted, name='deletecomplete'),
-    path('deleteall', views.deleteAll, name='deleteall'),
     url(r'^logout/$', auth_views.logout,{'next_page': '/'}, name='logout'),
     url(r'^login/$', auth_views.login, name='login'),
     url(r'$', views.index,name='index' ),
